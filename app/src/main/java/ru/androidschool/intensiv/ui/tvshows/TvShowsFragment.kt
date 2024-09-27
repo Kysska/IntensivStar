1656395b2f2c4df02b93ec87665b94a54284a374
Kysska <saifutdinovaila514@gmail.com>
2024-09-27 22:29:56 +1000

    add presenter(screen tv show)

--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -4,19 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import ru.androidschool.intensiv.databinding.TvShowsFragmentBinding
 import ru.androidschool.intensiv.domain.entity.MovieCard
 import ru.androidschool.intensiv.domain.usecase.GetMoviesUseCase
-import ru.androidschool.intensiv.domain.usecase.LoadMoviesUseCase
 import ru.androidschool.intensiv.ui.BaseFragment
-import ru.androidschool.intensiv.utils.MovieType
-import ru.androidschool.intensiv.utils.extensions.applyLoader
-import ru.androidschool.intensiv.utils.extensions.applySchedulers
-import timber.log.Timber
 
-class TvShowsFragment : BaseFragment() {
+class TvShowsFragment : BaseFragment(), TvShowPresenter.TvShowView {
 
     private var _binding: TvShowsFragmentBinding? = null
     private val binding get() = _binding!!
@@ -27,6 +23,10 @@ class TvShowsFragment : BaseFragment() {
 
     private val getMoviesUseCase: GetMoviesUseCase by lazy {
         GetMoviesUseCase(movieCardRepositoryImpl)
+    }
+
+    private val presenter: TvShowPresenter by lazy {
+        TvShowPresenter(getMoviesUseCase, this)
     }
 
     override fun onCreateView(
@@ -42,21 +42,27 @@ class TvShowsFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.tvshowRecyclerView.adapter = adapter
-
-        loadTvShowsMovieFromNetwork()
+        presenter.loadTvShow()
     }
 
-    private fun loadTvShowsMovieFromNetwork() {
-        compositeDisposable.add(
-            getMoviesUseCase.execute(MovieType.TV_SHOW)
-                .applySchedulers()
-                .applyLoader(binding.progressBarContainer.progressBar)
-                .subscribe({ movies ->
-                    updateTvShowList(movies)
-                }, { error ->
-                    Timber.e(error, "Error loading tv show")
-                })
-        )
+    override fun showTvShows(tvShow: List<MovieCard>) {
+        updateTvShowList(tvShow)
+    }
+
+    override fun showLoading() {
+        binding.progressBarContainer.progressBar.visibility = View.VISIBLE
+    }
+
+    override fun hideLoading() {
+        binding.progressBarContainer.progressBar.visibility = View.GONE
+    }
+
+    override fun showError() {
+        Toast.makeText(requireContext(), "Error loading TV shows", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showEmptyMovies() {
+        Toast.makeText(requireContext(), "No TV shows available", Toast.LENGTH_SHORT).show()
     }
 
     private fun updateTvShowList(tvShowList: List<MovieCard>) {
@@ -70,5 +76,6 @@ class TvShowsFragment : BaseFragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        presenter.clear()
     }
 }
